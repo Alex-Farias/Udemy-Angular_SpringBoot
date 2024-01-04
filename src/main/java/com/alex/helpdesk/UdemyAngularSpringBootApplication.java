77d80a607e109ac1c038ba28d8d1c63bfcc28f87
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2024-01-04 15:29:56 -0300

    initial commit

---
+++ src/main/java/com/alex/helpdesk/UdemyAngularSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alex.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyAngularSpringBootApplication.class, args);
+	}
+
+}
